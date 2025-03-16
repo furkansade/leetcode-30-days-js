41def303faef02d18ed2ea3e613a08c291e5860d
furkansade <20080069@stu.omu.edu.tr>
2025-03-16 15:57:08 +0300

    DAY-01: Create Hello World Function

---
+++ day-01/solution.js
@@ -0,0 +1,15 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        // solution
+        return 'Hello World'
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
